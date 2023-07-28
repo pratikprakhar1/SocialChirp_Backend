4e57ff9cd6a903a43de4ebbdfce7a26ca1cbdb3f
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-07-29 01:25:19 +0530

    Like feature added(toggling of like)

---
+++ src/repository/user-repository.js
@@ -0,0 +1,11 @@
+import User from "../models/user.js";
+import CrudRepository from "./crud-repository.js";
+class UserRepository extends CrudRepository{
+    constructor(){
+        super(User);
+    }
+    
+}
+
+
+export default UserRepository;
---
+++ src/services/like_service.js
@@ -0,0 +1,44 @@
+import {LikeRepository,TweetRepository} from '../repository/index.js'
+
+class LikeService{
+    constructor(){
+        this.likeRepository= new LikeRepository();
+        this.tweetRepository = new TweetRepository();
+    }
+
+    async toggleLike(modelId,modelType,userId){ // /api/v1/likes/toggle?toggle?id= modelid&type = Tweet
+        if(modelType== 'Tweet'){
+            var likeable = await this.tweetRepository.find(modelId).populate({path:'likes'});
+            console.log(likeable);
+        }else if(modelType == 'Comment'){
+            //TODO
+        }else{
+            throw new Error('unknown model type');
+        }
+        const exists = await this.likeRepository.findByUserAndLikeable({
+            user: userId,
+            onModel: modelType,
+            likeable: modelId
+        });
+        console.log(exists)
+        if(exists){
+            likeable.likes.pull(exists.id);
+            await likeable.save();
+            //await exists.remove();
+            var isAdded = false;
+        } else{
+            const newLike = await this.likeRepository.create({
+                user: userId,
+                onModel: modelType,
+                likeable: modelId
+            });
+            likeable.likes.push(newLike);
+            await likeable.save();
+            var isAdded = true;
+        }
+
+        return isAdded;
+    } 
+}
+
+export default LikeService;

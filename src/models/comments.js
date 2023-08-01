8609afdb43d3c593cb2e542a02700ccdd9006a18
Pratik Prakhar <pratikprakhar@Pratiks-MacBook-Air.local>
2023-08-02 01:00:24 +0530

    updated comment model

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,15 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const commentScema = new mongoose.Schema({
-     content:{
+const commentSchema = new mongoose.Schema({
+    content: {
         type: String,
         required: true,
-     },
-     userEmail:{
-        type: String
-     },
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    },
+    
+}, {timestamps: true});
 
-},{timestamps: true });
-
-const Comment = mongoose.model('Comment', commentScema);
-export default Comment;   
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
